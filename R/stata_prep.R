68fd18c13f1f64d219ff9ff9788a1beb799ba12d
Eve Perry <eve.perry@ucsf.edu>
2023-07-13 10:49:59 -0700

    add stata_prep help

--- R/stata_prep.R
+++ R/stata_prep.R
@@ -1,3 +1,18 @@
+#' Prepare Data for Stata
+#'
+#' Renames variables that don't comply with Stata's naming rules:
+#'
+#' Stata names can only contain letters, digits, and underscores. Illegal
+#' characters are replaced with underscores.
+#'
+#' Stata names can't start with a digit. Names with leading digits are prepended
+#' with an underscore.
+#'
+#' Stata names can only be 32 characters long. Names are truncated to a maximum
+#' of 32 characters.
+#'
+#' @param data A tibble or data frame to prepare for Stata.
+#'
 #' @export
 bhhi_stata_prep <- function(data) {
   dplyr::rename_with(data, stata_safe_variable_names, everything())
