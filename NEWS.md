1bc3d43a7513498d719bf5d4e4084243395d06b5
Eve Perry <eve@eveperry.com>
2023-07-24 15:48:00 -0700

    verify redcap token

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# bhhitools 0.2.1
+
+* `bhhi_store_token()` now checks for comformability with REDCap API token specification.
+
 # bhhitools 0.2.0
 
 * Adds `bhhi_shared_drive()` which builds OS-independent file paths to the BHHI shared drive.
--- R/bhhi_store_token.R
+++ R/bhhi_store_token.R
@@ -10,5 +10,24 @@
 #' @examplesIf interactive()
 #' bhhi_store_token("REDCAP_API_TOKEN")
 bhhi_store_token <- function(token_name) {
-  keyring::key_set(token_name, prompt = "Enter token:")
+  token <- askpass::askpass("Enter REDCAP API token:")
+
+  check_redcap_token(token, call = rlang::current_env())
+
+  keyring::key_set_with_value(token_name, password = token)
+}
+
+check_redcap_token <- function(token, call) {
+  tryCatch(
+    error = function(cnd) {
+      cli::cli_abort(
+        message = c(
+          "x" = "The token you provided does not conform to the REDCap API token rules.",
+          "i" = "Please double check the token you provided and try again."
+        ),
+        call = call
+      )
+    },
+    REDCapR::sanitize_token(token)
+  )
 }
