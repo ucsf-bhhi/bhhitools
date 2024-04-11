c005d913f26b239b3bf50a65fa6055d964489788
Eve Perry <eve@eveperry.com>
2024-04-10 18:57:23 -0700

    use env var for test token

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,4 @@
-test_token = "**REMOVED**"
+test_token = Sys.getenv("BHHITOOLS_TEST_REDCAP_TOKEN")
 
 test_data = function(filename) {
   readRDS(testthat::test_path(file.path("test-data", filename)))
