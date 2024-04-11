fb16f5bf1479e272da905f5e95cccee49f5c917d
Eve Perry <eve@eveperry.com>
2024-04-10 18:26:28 -0700

    remove token

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 test_token = "**REMOVED**"
-=======
-test_token = "***REMOVED***"
->>>>>>> ***REMOVED***
 
 test_data = function(filename) {
   readRDS(testthat::test_path(file.path("test-data", filename)))
